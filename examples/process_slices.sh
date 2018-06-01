b4da0c74b1ab4bec0822423bc1f0babd17ff4e10
julius <j.juodakis@gmail.com>
2018-06-01 15:14:23 +0200

    working version of all wrappers used for BW analysis

--- examples/process_slices.sh
+++ examples/process_slices.sh
@@ -6,11 +6,13 @@
 
 # (Can also be moved into bw_defineslices.R)
 
-for chr in {1..22} "X"
-do
-	# starts 25 jobs of make_slice.sh for this chromosome
-	# passes a row from slicedef.txt as args 1-3
-	# and adds M or F as arg 4 to the job
-	parallel -j 25 --progress -a slicedef_moms_${chr}.txt --colsep ' ' ./make_slice.sh {} M
-	parallel -j 25 --progress -a slicedef_fets_${chr}.txt --colsep ' ' ./make_slice.sh {} F
-done
+cat slicedef_moms_{1..22}.txt > slicedef_moms_autosomes.txt
+cat slicedef_fets_{1..22}.txt > slicedef_fets_autosomes.txt
+
+# starts 25 jobs of make_slice.sh for each definition file
+# passes a row from slicedef.txt as args 1-3
+# and adds M or F as arg 4 to the job
+parallel -j 25 --progress -a slicedef_moms_autosomes.txt --colsep ' ' ./make_slice.sh {} M
+parallel -j 25 --progress -a slicedef_fets_autosomes.txt --colsep ' ' ./make_slice.sh {} F
+parallel -j 25 --progress -a slicedef_moms_X.txt --colsep ' ' ./make_slice.sh {} M
+parallel -j 25 --progress -a slicedef_fets_X.txt --colsep ' ' ./make_slice.sh {} F
