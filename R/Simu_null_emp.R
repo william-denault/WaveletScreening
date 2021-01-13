ceae19940a2fb791ff67b7b3801be982c396878b
WD <william-denault@users.noreply.github.com>
2021-01-13 13:37:06 +0100

    generating Markdown

--- R/Simu_null_emp.R
+++ R/Simu_null_emp.R
@@ -1,3 +1,4 @@
+
 #'@title Simulation of the null statistics
 #'@description  Simulation of the null statistics
 #'@param res an output of Wavelet_screening function. The user can also provide a matrix of results of Wavelet_screening (from the same analysis), where the results have been concatenated by row (rbind).
@@ -56,6 +57,101 @@ Simu_null_emp <- function(res,
 
 
 
+  max_EM_post_Beta <- function(my_betas, lev_res,null_sd,alt_sd,alp) {
+    niter = 100
+    epsilon <- 10^-4
+    p_vec <- c()
+    sd_vec <- c()
+    erreur<-1+epsilon
+    eps <-10^-10#slight correction in case of non identifiable mixture
+    #prevent from having division by 0 in update parameter sum(temp)
+    betasub = my_betas
+    m0.hat<-0
+    m1.hat<-0
+    sigma0.hat<- null_sd
+    sigma1.hat<-alt_sd
+    #Prevent from label swapping
+    if(sigma1.hat < sigma0.hat){
+      sigma1.hat <- 3*sigma0.hat+sigma1.hat
+    }
+
+    p.hat<-0.25
+    new.params<-c(m0.hat,m1.hat,sigma0.hat,sigma1.hat,p.hat)
+    erreur<-1+epsilon
+    iter <- 1
+
+    while((erreur>epsilon)&(iter<=niter))
+    {
+      old.log.lik<- sum(log(p.hat*dnorm( betasub ,m1.hat,sigma1.hat)+(1-p.hat)*dnorm( betasub ,m0.hat,sigma0.hat)))
+      old.params<-new.params
+      #vecteur des pi_{i1}^{t}
+      temp<-p.hat*dnorm( betasub ,m1.hat,sigma1.hat)/(p.hat*dnorm( betasub ,m1.hat,sigma1.hat)+(1-p.hat)*dnorm( betasub ,m0.hat,sigma0.hat))
+      #Update parameter
+      p.hat<-mean(temp)
+      m1.hat<-sum(temp* betasub)/(sum(temp)+eps)
+      #m0.hat<-sum((1-temp)* betasub)/(sum(1-temp)+eps)
+      sigma1.hat<-sqrt( sum(temp*( betasub-m1.hat)^2)/(sum(temp)+eps) )+alt_sd
+      sigma0.hat<-sqrt( sum((1-temp)*( betasub-m0.hat)^2)/(sum(1-temp)+eps) )
+      #limit the decrease of sigma0.hat in case of non identifiable mixture
+      if(sigma0.hat < 0.1*null_sd ){
+        sigma0.hat <- 0.1*null_sd
+      }
+      new.params<-c(m0.hat,m1.hat,sigma0.hat,sigma1.hat,p.hat)
+      #Check end
+      new.log.lik<- sum(log(p.hat*dnorm( betasub ,m1.hat,sigma1.hat)+(1-p.hat)*dnorm( betasub ,m0.hat,sigma0.hat)))
+      epsilon <- abs( new.log.lik -old.log.lik)
+      iter<-iter+1
+
+    }
+
+    #Proba Belong belong to the alternative:
+    pos.prob <- rep(NA,length(my_betas))
+    for (i in 1:length(my_betas))
+    {
+      pos.prob[i] <- p.hat*dnorm( my_betas[i] ,m1.hat,sigma1.hat)/(p.hat*dnorm( my_betas[i] ,m1.hat,sigma1.hat)+(1-p.hat)*dnorm( my_betas[i] ,m0.hat,sigma0.hat))
+
+    }
+
+    lambcom <- rep(NA,(lev_res+1))
+    p_vec   <- rep(NA,(lev_res+1))
+    for (gi in 0:lev_res) {
+
+      ####################################
+      #Soft Thresholding
+      ####################################
+      temp <- pos.prob[(2^gi):(2^(gi + 1) - 1)]- alp*((1/2^(gi -1))^(1-1/7))
+      pos.prob[(2^gi):(2^(gi + 1) - 1)] <-ifelse(temp<0,0, temp)
+      ####################################
+      #proportion of association per level
+      ####################################
+      p_vec[(gi+1)]    <-  mean(pos.prob[(2^gi):(2^(gi + 1) - 1)])
+      lambcom[(gi+1)]  <-  mean(pos.prob[(2^gi):(2^(gi + 1) - 1)]*dnorm( betasub[(2^gi):(2^(gi + 1) - 1)] ,m1.hat,sigma1.hat)-(1-pos.prob[(2^gi):(2^(gi + 1) - 1)])*dnorm( betasub[(2^gi):(2^(gi + 1) - 1)] ,m0.hat,sigma0.hat))
+    }
+    porth   <- rep(NA,(lev_res))
+    start <- 2^(1:lev_res)
+    end  <-2^((1+1):(lev_res+1))-1
+    for( gi in 0:(lev_res-1))
+    {
+      tempstart <- round(start + (gi)*(end-start)/lev_res)
+      tempend <-round(start + (gi+1)*(end-start)/lev_res)
+      ind <- c()
+      for ( j in 1:lev_res)
+      {
+        temp <- cbind(tempstart,tempend)
+        p1 <- temp[j,1]
+        p2 <- temp[j,2]
+        ind <- c(ind, p1:p2 )
+      }
+      porth[gi+1] <-  mean(pos.prob[ind])
+
+    }
+    ph <- sum(p_vec)
+    pv <- sum(porth)
+    min_ph_pv <- min( ph,pv)
+    L_h <- sum(lambcom)
+    return(c(L_h, min_ph_pv))
+  }
+
 
   ################################################
   #Computing robustified variance for the diagonal
@@ -86,10 +182,29 @@ Simu_null_emp <- function(res,
     emp_cov <-  var_sim * (emp_cov/(max(emp_cov)))
 
   }   else   {
-    emp_cov <- (cov(res [, grep( pattern = "Beta*", colnames(res))]))
-    subres <- res [, grep( pattern = "Beta*", colnames(res))]
-    temp <-  c(subres)
-    var_sim <- mad(temp)^2
+    N=1000
+    #Generate random signal
+    SNP=1:2^(lev_res+3)
+    bp= SNP
+    #Preparing for the wavelet transform
+    Time01 <- (bp- min(bp))/(max(bp)-min(bp))
+    loci<- matrix(runif(N*2^(lev_res+3),min=0,2),ncol=N)
+    for ( i in 1:N)
+    {
+      loci[,i]  <- rnorm(n = 2^(lev_res+3))
+
+
+    }
+
+    bp=1:2^(lev_res)
+    #Preparing for the wavelet transform
+    Gen_W_trans <- apply(loci,2,my_wavproc)
+    Gen_W_trans = apply(Gen_W_trans, 1, Quantile_transform)
+    emp_cov <- cov(Gen_W_trans)
+    emp_cov <-  (mad(c(res [, grep( pattern = "Beta*", colnames(res))]))^2)*emp_cov/(max(emp_cov))
+
+
+    var_sim <- (mad(c(res [, grep( pattern = "Beta*", colnames(res))]))^2)
     null_sd <-sqrt(var_sim)
   }
 
@@ -116,7 +231,7 @@ Simu_null_emp <- function(res,
   out <- list()
   my_f <- function(y)
   {
-    pis <- max_EM_post_Beta(y, lev_res = lev_res , null_sd = null_sd ,alt_sd = alt_sd,alp=alp)[[1]]
+    pis <- max_EM_post_Beta(y, lev_res = lev_res , null_sd = null_sd ,alt_sd = alt_sd,alp=alp)
     return(pis)
   }
   if(print==TRUE)
